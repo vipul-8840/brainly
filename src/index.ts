2af520f33d88bf2f612d44bd0cf75f0f0a039afe
vipul-8840 <vipulkumarlen@gmail.com>
2024-11-30 12:27:00 +0530

    added files

---
+++ src/index.ts
@@ -0,0 +1,40 @@
+
+import express from "express";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+const app = express();
+
+app.post("api/v1/signup",(req,res)=>
+{
+
+})
+app.post("api/v1/signup",(req,res)=>
+{
+
+})
+app.post("api/v1/signup",(req,res)=>
+{
+
+})
+app.post("api/v1/content",(req,res)=>
+{
+
+})
+
+app.get("api/v1/content",(req,res)=>
+{
+
+})
+app.delete("api/v1/content",(req,res)=>
+{
+
+})
+app.post("api/v1/brain/share",(req,res)=>
+{
+
+})
+app.post("api/v1/brain/:shareLink",(req,res)=>
+{
+
+})
+    
